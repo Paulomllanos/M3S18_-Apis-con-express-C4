69cf82fc1997ce13c9f0369300f3a05b551b9369
Paulo Maldonado <paulomllanos@gmail.com>
2023-03-02 21:08:09 -0300

    Jueves 02-03-2023

--- Controllers/productController.js
+++ Controllers/productController.js
@@ -1,3 +1,5 @@
+//* genera un id random para nuestros productos, que no se repite
+const { v4: uuid4 } = require('uuid')
 
 let products = [];
 
@@ -6,6 +8,7 @@ const createProduct = (req, res) => {
         const {name, price, isAvailable, stock } = req.body
 
         products.push({
+            id: uuid4(), 
             name,
             price,
             isAvailable,
@@ -26,12 +29,19 @@ const createProduct = (req, res) => {
 
 const getProducts = (req, res) => {
     try {
-        const result = products.map((product) => product)
-
-        res.json({
-            success: true,
-            message: result
-        })
+        console.log(process.env.CLAVE_DE_ACCESO)
+        const clave = req.headers.clave;
+        console.log(clave)
+        if(clave === process.env.CLAVE_DE_ACCESO){
+            res.json({
+                success: true,
+                message: "Has obtenido la informacion de todos los productos de la base de datos",
+                info: products
+            })
+        } else {
+            throw new Error("Te pasaste de pillin, vuelve a intentarlo mas tarde!!!!")
+        }
+        
     } catch (error) {
         res.json({
             success: false, message: error.message
@@ -39,4 +49,56 @@ const getProducts = (req, res) => {
     }
 }
 
-module.exports = { createProduct, getProducts }
+const editProduct = (req, res) => {
+    try {
+        //* capturar id que vienen en la url
+        const id = req.params.productId;
+
+        const {name, price, isAvailable, stock } = req.body;
+
+        const productEdit = {
+            name,
+            price,
+            isAvailable,
+            stock
+        };
+
+        products = products.map((product) => {
+            if(product.id === id){
+                return productEdit;
+            } else {
+                return product
+            }
+        })
+
+        res.json({
+            success: true,
+            message: "Producto editado",
+            info: products
+        })
+    } catch (error) {
+        res.json({success: false, message: error.message})
+    }
+    
+}
+
+const deleteProduct = (req, res) => {
+    try {
+        const id = req.params.productId;
+
+        if(!products.find((product) => product.id !== id)){
+            throw new Error("Producto no existe!!!")
+        } else {
+            products = products.filter((product) => product.id !== id); 
+                res.json({
+                    success: true,
+                    message: "Has elimanado un producto",
+                    info: products
+                })
+        }
+    } catch (error) {
+        res.json({success: false, message: error.message}) 
+    }
+}
+
+module.exports = { createProduct, getProducts, editProduct, deleteProduct }
--- Routes/ProductRoute.js
+++ Routes/ProductRoute.js
@@ -1,10 +1,17 @@
 const express = require('express');
-const { createProduct, getProducts } = require('../Controllers/productController') 
+const { createProduct, getProducts, editProduct, deleteProduct } = require('../Controllers/productController') 
 
 const productRouter = express.Router();
 
+//! endpoints
 productRouter.route('/products')
-    .get(getProducts)
+    .get(getProducts)    
     .post(createProduct)
+
+productRouter.route('/products/:productId')
+    .put(editProduct)
+    .delete(deleteProduct)
+
+
 
 module.exports = productRouter;
